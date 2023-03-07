214def848451f72389b0e21c09ab3258cf29394c
Taha <belkhiri.taha53@gmail.com>
2023-03-07 23:16:25 +0100

    finished linking frontend to api in menu section

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,137 +1,79 @@
 import './CreateGame.css';
 import React from 'react';
 
-import ReturnButton from './ReturnButton';
+function CreateGame(props) {
 
-
-
-
-class CreateGame extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({username: event.target.form[0].value});
-  }
-
-  /*createGame(event) {
-    this.setState({isGameStarted: true});
-    console.log("Creating game");
-
-    const url = 'https://localhost:7080/api/game/';
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    .then(response => 
-              if(response.status == 200){
-                                          await;
-                                        })
-    .then(data => {
-      // Manipuler les données de la réponse
-      console.log(data);
-    })
-    .then()
-    .catch(error => console.error('Error:', error))
-
-
-    if(responseCOde = 200){
-      const id_game = response.id;
-      const id_player = response.playerId;
-
-      //afficher l'id de la game
-      //mettre le joueur dans la game
-      url = 'https://localhost:7080/api/game/'+ id_game + '?playerId=' + id_player;
-      fetch(url, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
+    function handleCreateGame(event) {
+        event.preventDefault();
+        const usernameInput = document.getElementById('username');
+        const username = usernameInput.value;
+        const url = 'https://localhost:7080/api/game/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username })
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response;
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(response => response.text())
+            .then(data => {
+                props.gameIdCallBack(data);
+                fetch('https://localhost:7080/api/game/' + data + '?playerId=1', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ username })
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            return response;
+                        }
+                        throw new Error(response.text());
+                    })
+                    .catch(error => {
+                        alert(error);
+                        // dont execute the rest of the function
+                        return Promise.reject(error);})
+                    .then(response => response.text())
+                    .then(data => {
+                        props.playerIdCallBack(1);
+                    })
+                    .catch(error => alert(error));
+            })
+            .catch(error => alert(error));
     }
 
-  }*/
-
-  createGame(event) {
-    this.setState({isGameStarted: true});
-    console.log("Creating game");
-  
-    const url = 'https://localhost:7080/api/game/';
-  
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    .then(response => {
-      if(response.status == 200) {
-        return response.json(); // renvoie la réponse sous forme de JSON
-      } else {
-        throw new Error('Erreur lors de la création de la partie');
-      }
-    })
-    .then(data => {
-      const id_game = data.id;
-      const id_player = data.playerId;
-  
-      console.log('ID de la partie :', id_game);
-  
-      // Ajouter le joueur à la partie
-      const url_join = `https://localhost:7080/api/game/${id_game}?playerId=${id_player}`;
-      return fetch(url_join, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-    })
-    .then(response => {
-      if(response.status == 200) {
-        console.log('Le joueur a été ajouté à la partie');
-      } else {
-        throw new Error('Erreur lors de l\'ajout du joueur à la partie');
-      }
-    })
-    .catch(error => console.error('Error:', error));
-  }
-
-  render() {
     return (
-      <React.StrictMode>
-      <div>
-        <button className="button" type="button" onClick={ReturnButton}>
-          BACK
-        </button>
-      </div>
-      <div className = "Title">
-        <h1>SPATIAL WAR</h1>
-      </div>
-      <div className = "CreateGameForm">
-        <form id='create'>
-          <label>
-            <input type="text" placeholder='username' value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-        <button className="button" type="button" onClick={ this.createGame } >
-          CREATE
-        </button>
-      </div>
-      </React.StrictMode>
+        <>
+            <div>
+                <button className="button" type="button" onClick={props.returnButtonCallBack}>
+                    BACK
+                </button>
+            </div>
+            <div className="Title">
+                <h1>SPATIAL WAR</h1>
+            </div>
+            <div className="CreateGameForm">
+                <form id='create'>
+                    <label>
+                        <input type="text" id="username" placeholder='username' />
+                    </label>
+                    <br />
+                </form>
+                <button className="button" type="button" onClick={handleCreateGame}>
+                    CREATE
+                </button>
+            </div>
+        </>
     );
-  }
 }
-
 
 
 export default CreateGame;
--- src/components/JoinGame.js
+++ src/components/JoinGame.js
@@ -3,91 +3,175 @@ import React from 'react';
 
 import Container from '../Container';
 
-import ReturnButton from './ReturnButton'; 
+import ReturnButton from './ReturnButton';
+import { useState, useEffect } from 'react';
 
 
+function JoinGame(props) {
+    const [username, setUsername] = useState('');
+    const [gameid, setGameId] = useState('');
+    const [isGameStarted, setIsGameStarted] = useState(false);
 
-class JoinGame extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {username: '',
-                  gameid: '',
-                    isGameStarted: false};
+    function handleChange(event) {
+        setUsername(event.target.form[0].value);
+        setGameId(event.target.form[1].value);
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.joinGame = this.joinGame.bind(this);
-  }
+    function joinGame(event) {
+        setIsGameStarted(true);
+        console.log("Joining game");
 
-  handleChange(event) {
+        const url = 'https://localhost:7080/api/game/' + gameid + '?playerId=2';
+        console.log(url)
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(async response => {
+                if (response.ok) {
+                    return response;
+                }
+                const message = await response.text();
+                throw new Error(message);
+            })
+            .catch(error => {
+                alert(error);
+                // dont execute the rest of the function
+                return Promise.reject(error);})
+            .then(async response => {
+                const data = await response.text();
+                props.gameIdCallBack(data);
+                props.playerIdCallBack(2);
+            })
+            .catch(error => {
+                alert(error);
+            });  
+    }
 
-    this.setState({username: event.target.form[0].value,
-                    gameid: event.target.form[1].value});
-  }
 
-
-
-  joinGame(event) {
-    this.setState({isGameStarted: true});
-    console.log("Joining game");
-
-    const url = 'https://localhost:7080/api/game/' + this.state.gameid + '/' + this.state.username;
-    console.log(url)
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    .then(response =>
-              response.json())
-    .catch(error => console.error('Error:', error))
-
-  }
-
-  //'https://localhost:7080/api/game/'+ this.state.username, pour créer une game
-
-  render() {
-    if (this.state.isGameStarted) {
-      return (
-        <Container />
-      );
+    if (isGameStarted) {
+        return (
+            <Container />
+        );
     }
     return (
-      <React.StrictMode>
-      <div>
-        <button className="button" type="button" onClick={ReturnButton}>
-          BACK
-        </button>
-      </div>
-      <div className = "Title">
-        <h1>SPATIAL WAR</h1>
-      </div>
-      <div className = "JoinGameForm">
-        <form /*onSubmit={this.handleSubmit}*/>
-          <label>
-            <input type="text" 
-                    placeholder='Username' 
-                    value={this.state.username} 
-                    onChange={this.handleChange} />
-          </label>
-          <br/>
-          <label>
-            <input type="text"
-                   placeholder='GameId'
-                   value={this.state.gameid}
-                   onChange={this.handleChange} />
-          </label>
-          <br/>
-        </form>
-        <button className="button" type="button" onClick={ this.joinGame }>
-
-          JOIN
-        </button>
-      </div>
-      </React.StrictMode>
+        <React.StrictMode>
+            <div>
+                <button className="button" type="button" onClick={ReturnButton}>
+                    BACK
+                </button>
+            </div>
+            <div className="Title">
+                <h1>SPATIAL WAR</h1>
+            </div>
+            <div className="JoinGameForm">
+                <form /*onSubmit={this.handleSubmit}*/>
+                    <label>
+                        <input type="text"
+                            placeholder='Username'
+                            value={username}
+                            onChange={handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        <input type="text"
+                            placeholder='GameId'
+                            value={gameid}
+                            onChange={handleChange} />
+                    </label>
+                    <br />
+                    <input className="button" type="submit" value="Join" onClick={joinGame} />
+                </form>
+            </div>
+        </React.StrictMode>
     );
-  }
 }
+
+
+
+// class JoinGame extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {username: '',
+//                   gameid: '',
+//                     isGameStarted: false};
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.joinGame = this.joinGame.bind(this);
+//   }
+
+//   handleChange(event) {
+
+//     this.setState({username: event.target.form[0].value,
+//                     gameid: event.target.form[1].value});
+//   }
+
+
+
+//   joinGame(event) {
+//     this.setState({isGameStarted: true});
+//     console.log("Joining game");
+
+//     const url = 'https://localhost:7080/api/game/' + this.state.gameid + '/' + this.state.username;
+//     console.log(url)
+//     fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//     })
+//     .then(response =>
+//               response.json())
+//     .catch(error => console.error('Error:', error))
+
+//   }
+
+//   //'https://localhost:7080/api/game/'+ this.state.username, pour créer une game
+
+//   render() {
+//     if (this.state.isGameStarted) {
+//       return (
+//         <Container />
+//       );
+//     }
+//     return (
+//       <React.StrictMode>
+//       <div>
+//         <button className="button" type="button" onClick={ReturnButton}>
+//           BACK
+//         </button>
+//       </div>
+//       <div className = "Title">
+//         <h1>SPATIAL WAR</h1>
+//       </div>
+//       <div className = "JoinGameForm">
+//         <form /*onSubmit={this.handleSubmit}*/>
+//           <label>
+//             <input type="text" 
+//                     placeholder='Username' 
+//                     value={this.state.username} 
+//                     onChange={this.handleChange} />
+//           </label>
+//           <br/>
+//           <label>
+//             <input type="text"
+//                    placeholder='GameId'
+//                    value={this.state.gameid}
+//                    onChange={this.handleChange} />
+//           </label>
+//           <br/>
+//         </form>
+//         <button className="button" type="button" onClick={ this.joinGame }>
+
+//           JOIN
+//         </button>
+//       </div>
+//       </React.StrictMode>
+//     );
+//   }
+// }
 
 
 
